39c141508a91d0aa51e344d9f8d7c40a7c1fb55a
Taynan19 <taynan10gk@gmail.com>
2026-05-28 13:33:12 -0300

    l

--- biblioteca_taynan/html/index.php
+++ biblioteca_taynan/html/index.php
@@ -1,4 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Naymar Jr.</h1>
+
+    <a href="funcoes.php">Funções</a>
+    <a href="testes.php">Testes</a>
+
+
+</body>
+</html>
+
 <?php
     echo "Bem vindo<3";
-
+    echo "
+    <a href='funcoes.php'>Funções</a>
+    <a href='testes.php'>Testes</a>
+    ";
 ?>
